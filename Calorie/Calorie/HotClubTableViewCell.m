293e736fb6d93a12c185add2112f2f790e1baa38
杨凡 <799862527@qq.com>
2016-04-16 21:28:45 +0800

    首页UI

---
+++ Calorie/Calorie/HotClubTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  HotClubTableViewCell.m
+//  Calorie
+//
+//  Created by 杨凡 on 16/4/16.
+//  Copyright © 2016年 Hurricane. All rights reserved.
+//
+
+#import "HotClubTableViewCell.h"
+
+@implementation HotClubTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
