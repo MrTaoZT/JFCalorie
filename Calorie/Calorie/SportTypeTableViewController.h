fc4e6b8822f4cc4afad52fd1dc387de1de085256
Zly <a781683161@qq.com>
2016-04-17 09:50:58 +0800

    创建两个类

---
+++ Calorie/Calorie/SportTypeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SportTypeTableViewController.h
+//  Calorie
+//
+//  Created by Zly on 16/4/17.
+//  Copyright © 2016年 Hurricane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SportTypeTableViewController : UITableViewController
+
+@end
