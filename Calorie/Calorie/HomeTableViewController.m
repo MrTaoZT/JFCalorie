284ef4d0820cc19d18d19f98056dc7e0b4b62b97
Zly <a781683161@qq.com>
2016-04-17 19:41:03 +0800

    temp

--- Calorie/Calorie/HomeTableViewController.m
+++ Calorie/Calorie/HomeTableViewController.m
@@ -61,6 +61,9 @@
     //获取附近热门会所
     //[self getHotClub];
     
+    //user
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -524,30 +527,6 @@
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
 */
 
