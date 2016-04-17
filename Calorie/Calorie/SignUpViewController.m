dda03380b7a03319c55dd4c33c37c72667be53ad
xyl <997565336@qq.com>
2016-04-17 09:44:18 +0800

    登录  注册 忘记密码2

--- Calorie/Calorie/SignUpViewController.m
+++ Calorie/Calorie/SignUpViewController.m
@@ -9,7 +9,7 @@
 #import "SignUpViewController.h"
 
 @interface SignUpViewController ()
-
+@property (strong,nonatomic) NSTimer *timer;
 @end
 
 @implementation SignUpViewController
@@ -48,10 +48,41 @@
     [super didReceiveMemoryWarning];
 }
 
+#pragma mark - Action
+
 - (IBAction)signUpAction:(UIButton *)sender forEvent:(UIEvent *)event {
 
 }
 - (IBAction)codeAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    
+    NSDictionary *dic = @{@"userTel":_phoneTF.text,
+                        @"type":@1,};
+    //判断用户是否输入手机号  再判断用户手机号是否为11位
+    if (_phoneTF.text.length == 0) {
+        [Utilities popUpAlertViewWithMsg:@"请输入您的手机号" andTitle:nil onView:self];
+        return;
+    }
+    if (_phoneTF.text.length == 11) {
+        [RequestAPI getURL:@"/register/verificationCode" withParameters:dic success:^(id responseObject) {
+            if ([responseObject[@"resultFlag"] integerValue] == 8001) {
+                //定时器
+            }
+        } failure:^(NSError *error) {
+            NSLog(@"error = %@",[error userInfo]);
+            [Utilities popUpAlertViewWithMsg:@"服务器繁忙，请稍后再试！" andTitle:nil onView:self];
+        }];
+    }else{
+        [Utilities popUpAlertViewWithMsg:@"手机号码的位数必须为11位" andTitle:nil onView:self];
+    }
+}
+
+#pragma mark - Timer
+
+- (void)setTime{
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(changeTime) userInfo:nil repeats:YES];
+}
+
+- (void)changeTime{
     
 }
 @end
