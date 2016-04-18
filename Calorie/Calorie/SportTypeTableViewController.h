fe867e1cd6a09b1d7be401ef4cc60dffc2384287
Zly <a781683161@qq.com>
2016-04-18 16:00:47 +0800

    下拉刷新

--- Calorie/Calorie/SportTypeTableViewController.h
+++ Calorie/Calorie/SportTypeTableViewController.h
@@ -10,7 +10,9 @@
 
 @interface SportTypeTableViewController : UITableViewController
 
+@property(nonatomic, strong)NSString *sportName;
 @property(nonatomic, strong)NSString *sportType;
+@property(nonatomic, strong)NSString *city;
 
 @property(nonatomic)CGFloat setJing;
 @property(nonatomic)CGFloat setWei;
