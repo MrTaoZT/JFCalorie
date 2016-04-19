b86a584a5aff09814e490b33faabcfdc8103b074
xyl <997565336@qq.com>
2016-04-19 12:07:19 +0800

    my  collect3

--- Calorie/Calorie/MyCollectViewController.m
+++ Calorie/Calorie/MyCollectViewController.m
@@ -23,7 +23,17 @@
     _locMgr=[[CLLocationManager alloc]init];
     //设置代理
     _locMgr.delegate=self;
+    NSLog(@"00");
     
+    //判断用户定位服务是否开启
+    if ([CLLocationManager locationServicesEnabled]) {
+        //开始定位用户的位置
+        [_locMgr startUpdatingLocation];
+//        //每隔多少米定位一次（这里的设置为任何的移动）
+//        _locMgr.distanceFilter=kCLDistanceFilterNone;
+//        //设置定位的精准度，一般精准度越高，越耗电（这里设置为精准度最高的，适用于导航应用）
+//        _locMgr.desiredAccuracy=kCLLocationAccuracyBestForNavigation;
+        }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,26 +43,33 @@
 #pragma mark-GetUserCollect
 
 - (void) getUserCoolect{
+    NSLog(@"0");
     NSString *memberId = [[StorageMgr singletonStorageMgr]objectForKey:@"memberId"];
     NSDictionary *dic = @{@"memberId":memberId,
                          @"jing":@(jing),
                          @"wei":@(wei),
-                         @"favouriteId":@1,
+                         @"favouriteId":@1
                             };
-    [RequestAPI getURL:@"/mySelfController/memberScore" withParameters:dic success:^(id responseObject) {
+    
+    [RequestAPI getURL:@"/mySelfController/getMyCollection" withParameters:dic success:^(id responseObject) {
         NSLog(@"obj === %@",responseObject);
-        if ([responseObject[@"resultFlag"]]) {
-            <#statements#>
+        
+        if ([responseObject[@"resultFlag"] integerValue] == 8001) {
+//            NSDictionary *result = responseObject[@"result"];
+        }else {
+            NSLog(@"错误码   修改！");
         }
         
     } failure:^(NSError *error) {
-        [Utilities popUpAlertViewWithMsg:@"系统繁忙,定位失败" andTitle:nil onView:self];
+//        [Utilities popUpAlertViewWithMsg:@"系统繁忙,定位失败" andTitle:nil onView:self];
+        NSLog(@"error = %@",[error userInfo]);
     }];
 }
 
 #pragma mark-CLLocationManagerDelegate
 
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    NSLog(@"3");
     //locations数组里边存放的是CLLocation对象，一个CLLocation对象就代表着一个位置
     CLLocation *loc = [locations firstObject];
     
@@ -62,17 +79,21 @@
     wei = loc.coordinate.latitude;
     NSLog(@"latitude = %f",loc.coordinate.latitude);
     NSLog(@"longitude = %f",loc.coordinate.longitude);
+    
     NSString *longitude = [NSString stringWithFormat:@"%f",jing];
     NSString *latitude = [NSString stringWithFormat:@"%f",wei];
+    
     if (longitude.length == 0 || latitude.length == 0) {
-        jing = 120;
-        wei = 120;
+        NSLog(@"1");
+        jing = 120.3;
+        wei = 31.57;
         [self getUserCoolect];
     }else{
+        NSLog(@"2");
         [self getUserCoolect];
     }
     //停止更新位置（如果定位服务不需要实时更新的话，那么应该停止位置的更新）
-//    [_locMgr stopUpdatingLocation];
+    [_locMgr stopUpdatingLocation];
 }
 
 /** 定位服务状态改变时调用*/
